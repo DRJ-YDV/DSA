baac2425bb12b1cdd7764c37862ae4d94707bc98
DRJ-YDV <dheeraj274501yadav@gmail.com>
2026-02-03 11:37:27 +0530

    Time: 0 ms (100.00%) | Memory: 27.6 MB (69.85%) - LeetSync

---
+++ 3952-trionic-array-i/trionic-array-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isTrionic(vector<int>& nums) {
+        int n = nums.size();
+        int i=0;
+        while(i<n-1 && nums[i]<nums[i+1]){
+            i++;
+        }
+        if(i==0) return false;
+        int j=i;
+        while(j<n-1 && nums[j] > nums[j+1]){
+            j++;
+        }
+        if(j==i || j==n-1) return false;
+
+        while(j< n-1 && nums[j]<nums[j+1]){
+            j++;
+        }
+
+        return j == n-1;
+    }
+};
