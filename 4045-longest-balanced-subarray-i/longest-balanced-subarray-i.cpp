df018383ec0bd213621a3cd8272cacca7d68f8eb
DRJ-YDV <dheeraj274501yadav@gmail.com>
2026-02-10 19:54:02 +0530

    Time: 2033 ms (33.34%) | Memory: 522.3 MB (48.01%) - LeetSync

---
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestBalanced(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            unordered_set<int> vis;
+            int cnt[2] = {0, 0}; // cnt[0] = distinct evens, cnt[1] = distinct odds
+            for (int j = i; j < n; ++j) {
+                if (!vis.count(nums[j])) {
+                    vis.insert(nums[j]);
+                    cnt[nums[j] % 2]++; // increment odd/even count
+                }
+                if (cnt[0] == cnt[1]) {
+                    ans = max(ans, j - i + 1);
+                }
+            }
+        }
+        return ans;
+    }
+};
