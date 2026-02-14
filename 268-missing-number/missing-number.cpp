63829f4b630fcfc2174ddf5d50b1a3a609735bec
DRJ-YDV <dheeraj274501yadav@gmail.com>
2026-02-15 01:41:23 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (92.40%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        // sort(nums.begin(),nums.end());
+        int ans=0;
+        for(int i=1;i<=nums.size();i++){
+            ans ^= i^nums[i-1];
+        }
+        return ans;
+
+    }
+};
