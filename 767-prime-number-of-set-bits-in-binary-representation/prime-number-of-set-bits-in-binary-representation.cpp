e2a357de06251287124acbf8a63c6fcd67d586ff
DRJ-YDV <dheeraj274501yadav@gmail.com>
2026-02-21 14:53:25 +0530

    Time: 3 ms (72.17%) | Memory: 8 MB (35.12%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
+    bool isPrime(int n){
+        if(n<2) return false;
+        if(n==2) return true;
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0) return false;
+        }
+        return true;
+    }
 int countPrimeSetBits(int left, int right) {
-        int cnt = 0;
-        for (int i = left; i <= right; i++) {
-            string bit = "";
-            int num = i;
-            while(num!=0){
-                bit += num%2 + '0';
-                num /= 2;
-            }
-            int set = 0;
-            for(int j=0;j<bit.size();j++){
-                if(bit[j]=='1') set++;
-            }
-            if(set==2||set==3||set==5||set==7||set ==11||set==13||set==17||set==19) cnt++;
+        // int cnt = 0;
+        // for (int i = left; i <= right; i++) {
+        //     string bit = "";
+        //     int num = i;
+        //     while(num!=0){
+        //         bit += num%2 + '0';
+        //         num /= 2;
+        //     }
+        //     int set = 0;
+        //     for(int j=0;j<bit.size();j++){
+        //         if(bit[j]=='1') set++;
+        //     }
+        //     if(set==2||set==3||set==5||set==7||set ==11||set==13||set==17||set==19) cnt++;
+        // }
+        // return cnt;
+        int cnt=0;
+        for(int i=left;i<=right;i++){
+            if(isPrime(__builtin_popcount(i))) cnt++;
         }
         return cnt;
     }
