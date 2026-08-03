8aedf7396ff24ec531ef0b4910c85cc4f0e20a74
DRJ-YDV <dheeraj274501yadav@gmail.com>
2026-08-03 22:39:49 +0530

    Time: 9 ms (8.03%) | Memory: 11.1 MB (8.93%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -5,24 +5,38 @@ public:
         return vowels.find(c)!=vowels.end();
     }
     string reverseVowels(string s) {
+        // int k=s.size()-1;
+        // for(int i=0;i<s.size() && i<=k;){
+        //     if(present(s[i])){
+        //         if(present(s[k])){
+        //             swap(s[i],s[k]);
+        //             k--;
+        //             i++;
+        //         }
+        //         else {
+        //             k--;
+        //         }
+        //     }
+        //     else {
+        //         i++;
+        //         if(!present(s[k])) k--;
+        //     }
+        // }
+        // return s;
+        string str = s;
         int k=s.size()-1;
-        for(int i=0;i<s.size() && i<=k;){
-            if(present(s[i])){
-                if(present(s[k])){
-                    swap(s[i],s[k]);
-                    k--;
-                    i++;
-                }
-                else {
-                    k--;
-                }
-            }
-            else {
+        int i=0;
+        while(i<k){
+            while(i<k && !present(str[i])){
                 i++;
-                if(!present(s[k])) k--;
             }
+            while(i<k && !present(str[k])){
+                k--;
+            }
+        swap(str[i],str[k]);
+         i++;
+         k--;
         }
-        return s;
-
+        return str;
     }
 };
