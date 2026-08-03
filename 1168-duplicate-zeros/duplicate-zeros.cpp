c4a6f4b9986d7badbd020fce288949328d498e90
DRJ-YDV <dheeraj274501yadav@gmail.com>
2026-08-03 23:30:13 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (92.32%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.cpp
+++ 1168-duplicate-zeros/duplicate-zeros.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
     void duplicateZeros(vector<int>& arr) {
-        int n= arr.size();
-        for(int i=0;i<n;i++){
-            if(arr[i]==0){
-                arr.insert(arr.begin()+i,0);
-                i++;
+        // int n= arr.size();
+        // for(int i=0;i<n;i++){
+        //     if(arr[i]==0){
+        //         arr.insert(arr.begin()+i,0);
+        //         i++;
+        //     }
+        // }
+        // while(arr.size()>n) arr.pop_back();
+
+        int n=arr.size();
+        int j = n + count(arr.begin(),arr.end(),0);
+        for(int i=n-1;i>=0;i--){
+            if(--j<n){
+                arr[j]=arr[i];
+            }
+            if(arr[i]==0 && --j<n){
+                arr[j]=0;
             }
         }
-        while(arr.size()>n) arr.pop_back();
 
     }
 };
