699c405735ac39ce4e4e884e42a211bd0792d75a
DRJ-YDV <dheeraj274501yadav@gmail.com>
2026-08-04 22:56:06 +0530

    Time: 0 ms (100.00%) | Memory: 33.8 MB (17.56%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -1,26 +1,38 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
+        // vector<int>ans;
+        // int x=INT_MIN;
+        // for(int i=0;i<nums.size();i++){
+        //      int max=nums[i];
+        //      if(max>x) x=max;
+        // }
+        // int y=INT_MAX;
+        // for(int i=0;i<nums.size();i++){
+        //      int max1=nums[i];
+        //      if(max1<y) y=max1;
+        // }
+        // for(int i=y;i<=x;i++){
+        //     ans.push_back(i);
+        // }
+        // vector<int>ans1;
+        // for(int i=0;i<ans.size();i++){
+        //     if(find(nums.begin(),nums.end(),ans[i])==nums.end()){
+        //         ans1.push_back(ans[i]);
+        //     }
+        // }
+        // return ans1 ;  
+
+        int mx= *max_element(nums.begin(),nums.end());
+        int mn = *min_element(nums.begin(),nums.end());
+
+        unordered_set<int>s(nums.begin(),nums.end());
         vector<int>ans;
-        int x=INT_MIN;
-        for(int i=0;i<nums.size();i++){
-             int max=nums[i];
-             if(max>x) x=max;
-        }
-        int y=INT_MAX;
-        for(int i=0;i<nums.size();i++){
-             int max1=nums[i];
-             if(max1<y) y=max1;
-        }
-        for(int i=y;i<=x;i++){
-            ans.push_back(i);
-        }
-        vector<int>ans1;
-        for(int i=0;i<ans.size();i++){
-            if(find(nums.begin(),nums.end(),ans[i])==nums.end()){
-                ans1.push_back(ans[i]);
+        for(int i=mn;i<=mx;i++){
+            if(s.find(i)==s.end()){
+                ans.push_back(i);
             }
-        }
-        return ans1 ;       
+        }     
+        return ans;
     }
 };
