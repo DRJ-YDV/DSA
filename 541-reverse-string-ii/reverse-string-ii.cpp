16687e4b0bcf1de07cd903def2cc7032b49863a4
DRJ-YDV <dheeraj274501yadav@gmail.com>
2026-08-05 21:38:39 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (45.82%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.cpp
+++ 541-reverse-string-ii/reverse-string-ii.cpp
@@ -3,7 +3,6 @@ public:
     string reverseStr(string s, int k) {
         if(s.size()==1) return s;
         for(int i=0;i<s.size();i=i+2*k){
-            if(i==s.size()-1) break;
             reverse(s.begin()+i,s.begin()+min(i+k,(int)s.size()));
         }
         return s;
