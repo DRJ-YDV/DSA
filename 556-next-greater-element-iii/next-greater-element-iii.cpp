2b6a7b56cc4ee599e08cf1f246cb88490e1f940e
DRJ-YDV <dheeraj274501yadav@gmail.com>
2026-08-06 19:07:20 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (85.55%) - LeetSync

---
+++ 556-next-greater-element-iii/next-greater-element-iii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int nextGreaterElement(int n) {
+        string s = to_string(n);
+        int len = s.size();
+
+        // Step 1: Find the first decreasing digit from the right
+        int i = len - 2;
+        while (i >= 0 && s[i] >= s[i + 1])
+            i--;
+
+        if (i < 0)
+            return -1;
+
+        // Step 2: Find the smallest digit greater than s[i]
+        int j = len - 1;
+        while (s[j] <= s[i])
+            j--;
+
+        swap(s[i], s[j]);
+
+        // Step 3: Reverse the suffix
+        reverse(s.begin() + i + 1, s.end());
+
+        long long ans = stoll(s);
+
+        if (ans > INT_MAX)
+            return -1;
+
+        return (int)ans;
+    }
+};
