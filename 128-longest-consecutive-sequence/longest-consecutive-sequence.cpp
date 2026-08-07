ffedb691c303551a68d4b4c451fe6bdf81fc3428
DRJ-YDV <dheeraj274501yadav@gmail.com>
2026-08-07 22:40:49 +0530

    Time: 16 ms (87.18%) | Memory: 60.1 MB (92.97%) - LeetSync

---
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        // unordered_map<int,bool>map;
+        // for(int i=0;i<nums.size();i++){
+        //     map[nums[i]]=true;
+        // }
+        // for(int i=0;i<nums.size();i++){
+        //     if(map.count(nums[i]-1)>0){
+        //         map[nums[i]]=false;
+        //     }
+        // }
+        // int max=0;
+        // for(int i=0;i<nums.size();i++){
+        //     if(map[nums[i]]==true){
+        //         int j=0,count=0;
+        //         while(map.count(nums[i]+j)>0){
+        //             j++;
+        //             count++;
+        //         }
+        //         if(count>max) max=count;
+        //     }
+        // }
+        // return max;
+
+
+        int n = nums.size();
+        if(n == 0){
+            return 0;
+        }
+        sort(nums.begin(), nums.end());
+        int currentConsecutiveSequence = 1;
+        int longestConsecutiveSequence = 0;
+        for(int i=1; i<n; i++){
+            if(nums[i] != nums[i-1]){
+                if(nums[i] == nums[i-1] + 1){
+                    currentConsecutiveSequence++;
+                }
+                else{
+                    longestConsecutiveSequence = max(longestConsecutiveSequence, currentConsecutiveSequence);
+                    currentConsecutiveSequence = 1;
+                }
+            }
+        }
+        return max(longestConsecutiveSequence, currentConsecutiveSequence);
+   }
+};
