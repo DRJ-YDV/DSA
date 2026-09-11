bb122e5df8cc7d4b97952cb49160b9cae7c2dabb
DRJ-YDV <dheeraj274501yadav@gmail.com>
2026-09-11 19:58:55 +0530

    Time: 0 ms (100.00%) | Memory: 28.7 MB (93.75%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
@@ -1,19 +1,37 @@
 class Solution {
 public:
     int totalNumbers(vector<int>& digits) {
-        unordered_set<int>mp;
-        for(int i=0;i<digits.size();i++){
-            if(digits[i]==0) continue;
-            for(int j=0;j<digits.size();j++){
-                if(i==j) continue;
-                for(int k=0;k<digits.size();k++){
-                    if(k==i || k==j) continue;
-                    if(digits[k]%2!=0) continue;
-                    int num = digits[i]*100+digits[j]*10+digits[k];
-                    mp.insert(num);
+        // unordered_set<int>mp;
+        // for(int i=0;i<digits.size();i++){
+        //     if(digits[i]==0) continue;
+        //     for(int j=0;j<digits.size();j++){
+        //         if(i==j) continue;
+        //         for(int k=0;k<digits.size();k++){
+        //             if(k==i || k==j) continue;
+        //             if(digits[k]%2!=0) continue;
+        //             int num = digits[i]*100+digits[j]*10+digits[k];
+        //             mp.insert(num);
+        //         }
+        //     }
+        // }
+        // return mp.size();
+
+        int freq[10]={0};
+        for(int d:digits) freq[d]++;
+        int cnt=0;
+        for(int h=1;h<=9;h++){
+            if(freq[h]==0) continue;
+            freq[h]--;
+            for(int t=0;t<=9;t++){
+                if(freq[t]==0) continue;
+                freq[t]--;
+                for(int u=0;u<=8;u+=2){
+                    if(freq[u]>0) cnt++;
                 }
+                freq[t]++;
             }
+            freq[h]++;
         }
-        return mp.size();
+        return cnt;
     }
 };
