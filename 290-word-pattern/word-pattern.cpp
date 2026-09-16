52b18bc1a53f4a31e99a6604dd52211395f5bc55
DRJ-YDV <dheeraj274501yadav@gmail.com>
2026-09-16 18:54:26 +0530

    Time: 2 ms (15.38%) | Memory: 8.9 MB (39.90%) - LeetSync

---
+++ 290-word-pattern/word-pattern.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        unordered_map<char,string>mp;
+        unordered_map<string,char>rev;
+
+        stringstream ss(s);
+        vector<string>words;
+        string word;
+        while(ss >> word){
+            words.push_back(word);
+        }
+        if(pattern.size()!=words.size()) return false;
+        for(int i=0;i<pattern.size();i++){
+            if(mp.count(pattern[i]) && mp[pattern[i]]!=words[i]) return false;
+            if(rev.count(words[i]) && rev[words[i]] != pattern[i]) return false;
+
+            mp[pattern[i]] = words[i];
+            rev[words[i]] = pattern[i];
+        }
+        return true;
+    }
+};
